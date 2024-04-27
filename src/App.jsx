66b4c0201a7dd8f25cf94018ccbe64018a92b8f0
Adonis <adonis.casado@outlook.com>
2024-04-27 19:39:13 -0400

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from "./App.module.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import UserProfile from "./pages/UserProfile/UserProfile.jsx"
+import Login from "./pages/Login/Login.jsx";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route path="/" element={<Hero />} />
           <Route path="/race" element />
           <Route path="/user" element={<UserProfile />} />
-          <Route path="/login" element />
+          <Route path="/login" element={<Login />} />
           <Route path="/horses" element />
         </Routes>
       </div>
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import styles from './Login.module.css';
+
+function Login() {
+  return (
+    <div>
+      <h1 className={styles.title}>Login</h1>
+      <form action=''>
+        <div>
+          <input type='email' />
+          <label htmlFor='' >Your Email</label>
+        </div>
+        <div>
+          <input type='email' />
+          <label htmlFor='' >Your Email</label>
+        </div>
+        <button type='submit'>Login</button>
+        <div>
+          <span>New Here? <Link to='/register'>Create an account</Link></span>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login
