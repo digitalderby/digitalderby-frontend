bc0a8f8196cdec4b8c7bebdf89d90263f1cd7ad7
dfergusbrown <dfergusbrown@gmail.com>
2024-04-27 15:56:05 -0400

    installs react router and updates main.jsx and app.jsx structure to include routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import styles from './App.module.css'
-import Hero from './components/Hero/Hero';
-import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from "react-router";
+import styles from "./App.module.css";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
   return (
     <>
-    <div className={styles.App}>
-      <Navbar />
-      <Hero />
-    </div>
+      <div className={styles.App}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/race" element />
+          <Route path="/user" element />
+          <Route path="/login" element />
+          <Route path="/horses" element />
+        </Routes>
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
