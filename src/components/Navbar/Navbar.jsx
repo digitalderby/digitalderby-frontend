bf5b0b7e989d85f38ae7fdfd161d0e5d32be2195
Adonis <adonis.casado@outlook.com>
2024-04-26 23:51:41 -0400

    add hero

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">DIGITAL DERBY</a>
+      <a className={styles.title} href="/">Digitalderby</a>
       <div className={styles.menu}>
         <img 
           className={styles.menuBtn} 
