273d109fd38ba81c7163804f666de7ab72d3bcc7
dfergusbrown <dfergusbrown@gmail.com>
2024-04-28 11:18:24 -0400

    login toggles to register

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,35 @@
 import { Link } from 'react-router-dom';
 import styles from './Login.module.css';
+import { useState } from 'react';
+
 
 function Login() {
+  const [registering, setRegistering] = useState(false)
+
+  function handleToggleRegister() {
+    registering ? setRegistering(false) : setRegistering(true)
+  }
+
+  
   return (
     <div className={styles.authForm}>
-      <h1 className={styles.title}>Login</h1>
+      <h1 className={styles.title}>{registering ? "Register" : "Login"}</h1>
       <form action=''>
         <div className={styles.email}>
-          <input type='email' />
-          <label htmlFor='' >Your Email</label>
+          <input type='text' name='username'/>
+          <label htmlFor='username' >Username</label>
         </div>
         <div className={styles.password}>
-          <input type='password' />
-          <label htmlFor='' >Password</label>
+          <input type='password' name='password'/>
+          <label htmlFor='password' >Password</label>
         </div>
-        <button className={styles.loginBtn} type='submit'>Login</button>
+        <button className={styles.loginBtn} type='submit'>{registering ? "Submit User" : "Login"}</button>
         <div>
-          <span className={styles.register}>New here? <Link to='/register'>Create an account</Link></span>
+          {
+            registering
+            ? <span className={styles.register}>Already have an account? <a onClick={handleToggleRegister}>Log in instead</a></span>
+            : <span className={styles.register}>New here? <a onClick={handleToggleRegister}>Create an account</a></span>
+          }
         </div>
       </form>
     </div>
