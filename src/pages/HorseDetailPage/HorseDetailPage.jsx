ebbd42ec5fb9d56136844a7959eaadaa31af8bb6
Adonis <adonis.casado@outlook.com>
2024-04-29 22:01:23 -0400

    add horses detail from backend

--- src/pages/HorseDetailPage/HorseDetailPage.jsx
+++ src/pages/HorseDetailPage/HorseDetailPage.jsx
@@ -1,28 +1,43 @@
 import { useParams } from 'react-router-dom';
 import useHorseDetails from '../../hooks/useHorseDetails';
+import { useNavigate } from 'react-router-dom';
+import styles from './HorseDetailPage.module.css'
+
 
 function HorseDetailPage() {
     const { id } = useParams();
     const { horse, loading, error } = useHorseDetails(id);
+    const navigate = useNavigate();
+
+    const handleRedirect = () => {
+        navigate('/horses');
+    };
 
     if (loading) return <p>Loading horse details...</p>;
-    if (error) return <p>Error loading details: {error.message}</p>; // Display the error message
-
+    if (error) return <p>Error loading details: {error.message}</p>; 
     return (
-        <div>
-            <h1>Horse Details: {horse?.name}</h1> {/* Optional chaining */}
-            <p>Color: {horse?.color}</p> {/* Optional chaining */}
-            <p>Icons: {horse?.icons && horse.icons.map(icon => <span key={icon}>{icon}</span>)}</p> {/* Optional chaining */}
-            <div>
-                <h2>Stats:</h2>
+        <div className={styles.container}>
+            <h1 className={styles.title}>{horse?.name}</h1> 
+            <div className={styles.statItem}>
+                <span className={styles.statLabel}>Color: </span>
+                <span className={styles.statValue}>{horse?.color}</span>
+            </div> 
+            <div className={styles.statItem}>
+                <span className={styles.statLabel}>Icons: </span>
+                {horse?.icons && horse.icons.map(icon => <span key={icon} className={styles.statValue}>{icon}</span>)}
+            </div> 
+            <div className={styles.statItem}>
+                <h2 className={styles.statLabel}>Stats:</h2>
                 <ul>
-                    <li>Top Speed: {horse?.stats?.topSpeed}</li> {/* Optional chaining */}
-                    <li>Stamina: {horse?.stats?.stamina}</li> {/* Optional chaining */}
-                    <li>Acceleration: {horse?.stats?.acceleration}</li> {/* Optional chaining */}
+                    <li>Top Speed: {horse?.stats?.topSpeed}</li> 
+                    <li>Stamina: {horse?.stats?.stamina}</li> 
+                    <li>Acceleration: {horse?.stats?.acceleration}</li> 
                 </ul>
             </div>
+            <button onClick={handleRedirect}>Return</button>
         </div>
     );
 }
 
 export default HorseDetailPage;
+
