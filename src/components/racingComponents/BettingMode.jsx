0fdebe98e84c9f0bdaee40bfcd003bcaf55d9e83
Adonis <adonis.casado@outlook.com>
2024-04-30 16:46:09 -0400

    add results page json

--- src/components/racingComponents/BettingMode.jsx
+++ src/components/racingComponents/BettingMode.jsx
@@ -1,37 +1,13 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 
 const BettingMode = ({gameState}) => {
   
-  // useEffect(() => {
-  //   console.log(gameState)
-  // }, [gameState])
-  let prettifiedGameState = gameState
-
+  useEffect(() => {
+    console.log(gameState)
+  }, [gameState])
   return (
-    <div className="results-container">
-      <h1>Race Results</h1>
-      {winner ? (
-        <div className="winner">
-          <h2>Winner: {winner.name}</h2>
-          <p>Details:</p>
-          <ul>
-            <li>Speed: {winner.speed}</li>
-            <li>Time: {winner.time}s</li>
-          </ul>
-        </div>
-      ) : (
-        <p>No results available.</p>
-      )}
-      <h3>All Participants:</h3>
-      <ol>
-        {horses.map((horse, index) => (
-          <li key={index}>
-            {horse.name} - Finished with a time of {horse.time}s
-          </li>
-        ))}
-      </ol>
-    </div>
-  );
-};
+    <div>BettingMode</div>
+  )
+}
 
-export default ResultsMode;
+export default BettingMode
