2f2a4be2ef5c28c9151454bcdf63497dd7725ece
dfergusbrown <dfergusbrown@gmail.com>
2024-05-01 18:42:53 -0400

    adds animated background

--- src/components/racingComponents/RaceMode.jsx
+++ src/components/racingComponents/RaceMode.jsx
@@ -2,28 +2,33 @@ import React, { useEffect } from 'react'
 
 const RaceMode = ({gameState}) => {
   // useEffect(() => {
-  //   console.log(gameState)
-  // }, [gameState])
-  let prettifiedGameState = gameState
-
+    // console.log(gameState)
+  // }, [gameState]) 
+  // let racingTime = gameState.raceStates?.time
   return (
     <>
       <div>RaceMode</div>
       <div className='h-full flex justify-center items-center'>
-        <div className="" id='raceBackground'>
-          <div>
-            {
-              gameState?.raceStates?.horseStates?.map((horse, idx) => {
-                //horseStates[idx]
-                return <div key={idx} style={{
-                  left: `${(horse.position /100) + 5}%`,
-                  top: `${(25 * idx) + 5}%`
-                }}
-                className='horseInRace'
-                >{horse.horse.spec.icons[0]}</div>
-              })
-            }
+        <div id='raceScrollContainer'>
+          <div 
+            id='raceBackground'
+            style={
+              gameState?.raceStates?.time > 0 
+              && !gameState?.raceStates?.horseStates.some(el => el.finishTime)
+              ? {animation:"slide 30s linear infinite"} 
+              : null}
+            >
           </div>
+              {
+                gameState?.raceStates?.horseStates?.map((horse, idx) => {
+                  return <div key={idx} style={{
+                    left: `${((horse.position /100) + 5)*.9}%`,
+                    top: `${(20 * idx) + 12}%`
+                  }}
+                  className='horseInRace'
+                  >{horse.horse.spec.icons[0]}</div>
+                })
+              }
         </div>
       </div>
     </>
