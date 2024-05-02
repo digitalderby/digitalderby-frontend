4668025c56a66368a3529896b90fbb06fd3548b6
dfergusbrown <dfergusbrown@gmail.com>
2024-05-02 15:42:07 -0400

    adds betting modal and renders according to gamestate.  Fixes issue of conditionally rendered react hooks by shifting jsx to another component

--- src/pages/RacePage/RacePage.jsx
+++ src/pages/RacePage/RacePage.jsx
@@ -1,9 +1,10 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import "./racePage.css"
 import { SocketContext } from '../../contexts/SocketContext';
 import BettingMode from '../../components/racingComponents/BettingMode';
 import RaceMode from '../../components/racingComponents/RaceMode';
 import ResultsMode from '../../components/racingComponents/ResultsMode';
+import { Button } from 'react-bootstrap';
 
 const RacePage = () => {
     //Pull in game state
@@ -13,24 +14,35 @@ const RacePage = () => {
         username,
         currentBet,
         betResults,
-    
+        user,
         connected
     } = useContext(SocketContext)
 
+    const [betMode, setBetMode] = useState(true);
+    // const [resultsMode, setResultsMode] = useState(false)
+    const handleClose = () => setBetMode(false);
+    const handleShow = () => setBetMode(true);
+
     switch (gameState?.status) {
-        case "betting":
-            return <BettingMode gameState={gameState}/>
+        case "betting": !betMode && handleShow()
             break;
-        case "race":
-            return <RaceMode gameState={gameState}/>
+        case "race": betMode && handleClose()
             break;
-        case "results":
-            return <ResultsMode gameState={gameState}/>
+        case "results": console.log('results mode')
             break;
-        default : 
-            return <ResultsMode gameState={gameState}/>
+        default : null
             break;
     }
+
+    return (
+        <>
+            <RaceMode gameState={gameState}/>
+            <BettingMode gameState={gameState} show={betMode} handleClose={handleClose} user={user}/>
+            <Button variant="primary" onClick={handleShow}>
+                Launch Betting Modal
+            </Button>
+        </>
+    )
 }
 
 export default RacePage
