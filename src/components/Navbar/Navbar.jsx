4fb7a5573101896aff50cbbcc9451ea109f67f0d
dfergusbrown <dfergusbrown@gmail.com>
2024-05-02 18:39:04 -0400

    user and login vs logout render conditionally based on auth user property

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import styles from './Navbar.module.css';
 import { getImageUrl } from '../../utils';
 import { NavLink } from 'react-router-dom';
 import Wallet from './Wallet';
+import { AuthContext } from '../../contexts/AuthContext';
+import { Nav } from 'react-bootstrap';
 
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
-
+  const { user, logout } = useContext(AuthContext)
   const handleLinkClick = () => {
     setMenuOpen(false);
   };
@@ -24,11 +26,17 @@ function Navbar() {
           onClick={() => setMenuOpen(!menuOpen)} 
         />
         <ul className={`${styles.menuitems} ${menuOpen ? styles.visible : styles.hidden}`}>
-          <NavLink to="/login" onClick={handleLinkClick}>Login</NavLink>
           <NavLink to="/race" onClick={handleLinkClick}>Next Race</NavLink>
           <NavLink to="/horses" onClick={handleLinkClick}>Horse Stats</NavLink>
-          <NavLink to="/user" onClick={handleLinkClick}>User</NavLink>
-          <NavLink to="/test" onClick={handleLinkClick}>Test</NavLink>
+          {
+            user && <NavLink to="/user" onClick={handleLinkClick}>User</NavLink>
+          }
+          {
+            user
+            ? <NavLink to="/" onClick={logout}>Logout</NavLink>
+            : <NavLink to="/login" onClick={handleLinkClick}>Login</NavLink>
+
+          }
         </ul>
       </div>
     </nav>
