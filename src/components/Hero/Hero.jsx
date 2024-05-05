9da0a0196a3b5f7a853fee7ef5e3dc5b7b49faab
Adonis <adonis.casado@outlook.com>
2024-05-05 15:18:53 -0400

    hotfix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,9 @@ function Hero() {
       <div className={styles.content}>
         <h1 className={styles.title}>Race to Glory with Digital Derby</h1>
         <p className={styles.description}>Where every race is an opportunity to win.</p>
-        <NavLink to="/race" className={styles.signupBtn} />
+        <NavLink to="/race" className={styles.signupBtn}>
+        Next Race
+        </NavLink>
       </div>
       <img src={getImageUrl('hero/HorseHero.png')} alt='hero image of horse' className={styles.heroImg}></img>
     </section>
